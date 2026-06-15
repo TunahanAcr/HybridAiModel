55f8362d63829d33a5f505edd0f3559a8c0a297b
Tunahan Acar <acartunahan55@gmail.com>
2026-06-15 12:10:53 +0300

    Siyah beyaz kalibrasyonu için fonksiyon ekledni beyaz siyah ölçümleri yapılınca referans değerlerini değiştiricem

--- dataPipeline.py
+++ dataPipeline.py
@@ -3,6 +3,27 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 
+BLACK_REF = np.array([100, 105, 98, 110, 100, 95, 102, 101, 99, 
+    100, 105, 98, 110, 100, 95, 102, 101, 99])  # Siyah referans
+
+WHITE_REF = np.array([1100, 1150, 1080, 1120, 1100, 1090, 1110, 1105, 1095, 1100, 1150, 1080, 1120, 1100, 1090, 1110, 1105, 1095])  # Beyaz referans
+
+def calibrate_data(ham_data_df, sensor_columns):
+    
+
+    df_calibrated = ham_data_df.copy()
+
+    pay = df_calibrated[sensor_columns] - BLACK_REF
+    payda = WHITE_REF - BLACK_REF
+
+    payda[payda == 0] = 1 # Bölme işleminde sıfır hatasını önlemek için payda sıfır olanları 1 yapıyoruz
+
+    df_calibrated[sensor_columns] = pay / payda
+
+    df_calibrated[sensor_columns] = df_calibrated[sensor_columns].clip(0.0, 1.0)
+
+    return df_calibrated
+
 class KumasSpektralDataset(Dataset):
 
     def __init__(self, hibrit_kumas_veriset):
@@ -10,11 +31,19 @@ class KumasSpektralDataset(Dataset):
         # Veriyi oku
         self.data = pd.read_csv(hibrit_kumas_veriset)
 
+        # # İlk 18 sütun isimlerini al
+        sensor_columns = self.data.columns[:18]  
+
         #18 sütunu al
-        X_ham = self.data.iloc[:, :18].values
+        calibrated_data = calibrate_data(self.data, sensor_columns)
+
+        # Kalibre edilmiş verileri Pytorch okuyabilsin diye Numpy array formatına çeviriyoruz
+        self.X = calibrated_data[sensor_columns].values
 
         # Label sütununu al
-        Y_ham = self.data.iloc[:, -1].astype(float).values
+        self.Y = self.data.iloc[:, -1].astype(float).values
+
+        print("Veri seti başarıyla yüklendi ve kalibre edildi.")
 
         # Veriyi normalize et
 
