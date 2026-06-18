001eb815b5cf1c082b4c92b84010b842bb91de47
Tunahan Acar <acartunahan55@gmail.com>
2026-06-18 21:39:50 +0300

    Basit bir datasetle modeli eğitip daha önce görmediği bir değerler test ediyoruz

--- neural_network_2.py
+++ neural_network_2.py
@@ -1,14 +1,21 @@
 # Constants
-TARGET = 10
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.001
 
  # Normalde random seçicez şimdilik kendim değer atadım
 old_w_1 = 1
 old_w_2 = 2
 old_b = -1
 
+# Ai dan aldığım basit bir dataset
+dataset = [
+    (6.0, 8.0, 9.5),  # Öğrenci 1 (İdeal): Güzel çalışmış, uykusunu tam almış. Yüksek puan.
+    (1.0, 9.0, 3.0),  # Öğrenci 2 (Tembel): Sadece uyumuş, hiç çalışmamış. Düşük puan.
+    (4.0, 6.0, 6.5),  # Öğrenci 3 (Ortalama): Ortalama bir çalışma ve uyku. Ortalama puan.
+    (10.0, 1.0, 4.0)  # Öğrenci 4 (ZOMBİ - TUZAK): İnanılmaz çok çalışmış ama uykusuzluktan sınavda bayılmış!
+]
 
 
+# Lineer katman
 def linear(x_1, x_2) : 
 
     z = old_w_1 * x_1 + old_w_2 * x_2 + old_b
@@ -16,21 +23,24 @@ def linear(x_1, x_2) :
     return z
 
 
-
+# Aktivasyon fonskiyonu
 def relu(z) :
 
+    # reLU eğer değer negatifse sıfır yapar pozitifse aynı bırakır
     h = z if z > 0 else 0
 
     return h
     
 
-
+# Outputumuz tek nöronlu bir ağ olduğu için burda işlem yok
 def output(h) :
 
-    return h
+    prediction = h
+
+    return prediction
 
 
-
+# Modelin öğreneceği datanın hangi yönde akacağını tanımlıyoruz
 def forward(x_1, x_2) : 
     
     z = linear(x_1, x_2)
@@ -40,18 +50,18 @@ def forward(x_1, x_2) :
     return z, h, prediction, x_1 , x_2, 
 
 
+# Kayıp hesaplama metodumuz türevde kolaylık açısından başına 1/2 attık
+def loss_function(target,prediction) :
 
-def loss_function(prediction) :
-
-    total_loss = 1/2 * (TARGET - prediction) ** 2
+    total_loss = 1/2 * (target - prediction) ** 2
 
     return total_loss
 
 
+# Zincir kuralı ile türev hesapları her parametre için ayrı ayrı kısmi türev ve gradient slopeların hesaplanması
+def chain_rule(x_1, x_2, z, prediction, target):
 
-def chain_rule(x_1, x_2, z, prediction):
-
-    total_loss_prime = prediction - TARGET
+    total_loss_prime = prediction - target
     
     h_prime = 1 if z > 0 else 0
 
@@ -67,11 +77,11 @@ def chain_rule(x_1, x_2, z, prediction):
     return gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b
 
 
+# Her parametre için step sizeların hesaplanıp güncel parametlerin hesaplanması
+def backpropagation(x_1, x_2, z,target, prediction, old_w_1, old_w_2, old_b):
 
-def backpropagation(x_1, x_2, z, prediction, old_w_1, old_w_2, old_b):
-
-    total_loss = loss_function(prediction)
-    gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b= chain_rule(x_1, x_2, z, prediction)
+    total_loss = loss_function(target,prediction)
+    gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b= chain_rule(x_1, x_2, z, prediction, target)
     step_size_w_1 = LEARNING_RATE * gradient_slope_w_1
     step_size_w_2 = LEARNING_RATE * gradient_slope_w_2
     step_size_b = LEARNING_RATE * gradient_slope_b
@@ -84,19 +94,36 @@ def backpropagation(x_1, x_2, z, prediction, old_w_1, old_w_2, old_b):
     return total_loss, gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b, old_w_1, new_w_1, old_w_2, new_w_2, old_b, new_b
 
 
-
-epochs = 5
+# Modeli 100 tur eğitiyoruz
+epochs = 100
 
 for epoch in range(epochs) :
-    z, h, prediction, x_1, x_2= forward(2,3)
+    # Her 1 turda her bütün dataseti sırayla gösteriyoruz
+    for i in range(len(dataset)):
+        
+        z, h, prediction, x_1, x_2= forward(dataset[i][0], dataset[i][1])
 
-    total_loss, gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b, old_w_1, new_w_1, old_w_2, new_w_2, old_b, new_b= backpropagation(x_1, x_2, z, prediction, old_w_1, old_w_2, old_b)
+        total_loss, gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b, old_w_1, new_w_1, old_w_2, new_w_2, old_b, new_b= backpropagation(x_1, x_2, z,dataset[i][2], prediction, old_w_1, old_w_2, old_b)
 
-    print(f"{epoch+1}. Tur hesaplanan prediction: {prediction} \n hesaplanan toplam hata: {total_loss} \n hesaplanan w_1 gradient slope: {gradient_slope_w_1} \n hesaplanan w_2 gradient slope: {gradient_slope_w_2} \n hesaplanan b gradient slope: {gradient_slope_b} \n hesaplanan   yeni w1 değeri: {new_w_1} \n hesaplanan   yeni w2 değeri: {new_w_2} \n hesaplanan   yeni b değeri: {new_b} ")
+        print(f"{epoch+1}. Tur {dataset[i]}. data için hesaplanan prediction: {prediction} \n hesaplanan toplam hata: {total_loss} \n hesaplanan w_1 gradient slope: {gradient_slope_w_1} \n hesaplanan w_2 gradient slope: {gradient_slope_w_2} \n hesaplanan b gradient slope: {gradient_slope_b} \n hesaplanan   yeni w1 değeri: {new_w_1} \n hesaplanan   yeni w2 değeri: {new_w_2} \n hesaplanan   yeni b değeri: {new_b} ")
 
-    old_w_1 = new_w_1
-    old_w_2 = new_w_2
-    old_b = new_b
+        old_w_1 = new_w_1
+        old_w_2 = new_w_2
+        old_b = new_b
+
+
+print("--------------------------------------------------")
+print("Tek bir data ile eğitilmiş modeli test ediyoruz")
+
+# Modelin daha önce görmediği iki input
+new_x_1 = 20
+new_x_2 = 0
+
+# Test aşamasında sadece forward metodunu çağırıyoruz
+test_z, test_h, test_prediction, _, _ = forward(new_x_1, new_x_2)
+
+print(f"Gelen Yeni Veriler -> x_1: {new_x_1}, x_2: {new_x_2}")
+print(f"Eğitilmiş Modelin Tahmini: {test_prediction}")
 
 
 
