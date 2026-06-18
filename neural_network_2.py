f12876b215e1cfd699f8b73b6600752ff1338386
Tunahan Acar <acartunahan55@gmail.com>
2026-06-18 23:04:25 +0300

    2 nöronlu 9 parametreli neural network

--- neural_network_2.py
+++ neural_network_2.py
@@ -1,10 +1,21 @@
+import random
+
 # Constants
 LEARNING_RATE = 0.001
 
- # Normalde random seçicez şimdilik kendim değer atadım
-old_w_1 = 1
-old_w_2 = 2
-old_b = -1
+# 0 1 aralığından random başlangıç değerleri atıyoruz
+old_w_1 = random.uniform(-0.5, 0.5)
+old_w_2 = random.uniform(-0.5, 0.5)
+old_b_1 = random.uniform(-0.5, 0.5)
+
+old_w_3 = random.uniform(-0.5, 0.5)
+old_w_4 = random.uniform(-0.5, 0.5)
+old_b_2 = random.uniform(-0.5, 0.5)
+
+old_w_5 = random.uniform(-0.5, 0.5)
+old_w_6 = random.uniform(-0.5, 0.5)
+old_b_3 = random.uniform(-0.5, 0.5)
+
 
 # Ai dan aldığım basit bir dataset
 dataset = [
@@ -15,12 +26,22 @@ dataset = [
 ]
 
 
-# Lineer katman
-def linear(x_1, x_2) : 
+# Lineer katman 2 nöron 2 input
 
-    z = old_w_1 * x_1 + old_w_2 * x_2 + old_b
+def neuron_1(x_1, x_2) : 
 
-    return z
+    z_1 = old_w_1 * x_1 + old_w_2 * x_2 + old_b_1
+
+    return z_1
+
+
+
+def neuron_2(x_1, x_2) : 
+
+    z_2 = old_w_3 * x_1 + old_w_4 * x_2 + old_b_2
+
+    return z_2
+
 
 
 # Aktivasyon fonskiyonu
@@ -33,9 +54,9 @@ def relu(z) :
     
 
 # Outputumuz tek nöronlu bir ağ olduğu için burda işlem yok
-def output(h) :
+def output(h_1, h_2) :
 
-    prediction = h
+    prediction = old_w_5 * h_1 + old_w_6 * h_2 + old_b_3
 
     return prediction
 
@@ -43,11 +64,15 @@ def output(h) :
 # Modelin öğreneceği datanın hangi yönde akacağını tanımlıyoruz
 def forward(x_1, x_2) : 
     
-    z = linear(x_1, x_2)
-    h = relu(z)
-    prediction = output(h)
+    z_1 = neuron_1(x_1, x_2)
+    z_2 = neuron_2(x_1, x_2)
+    h_1 = relu(z_1)
+    h_2 = relu(z_2)
+    
 
-    return z, h, prediction, x_1 , x_2, 
+    prediction = output(h_1, h_2)
+
+    return z_1, h_1, z_2, h_2, prediction, x_1 , x_2, 
 
 
 # Kayıp hesaplama metodumuz türevde kolaylık açısından başına 1/2 attık
@@ -59,57 +84,109 @@ def loss_function(target,prediction) :
 
 
 # Zincir kuralı ile türev hesapları her parametre için ayrı ayrı kısmi türev ve gradient slopeların hesaplanması
-def chain_rule(x_1, x_2, z, prediction, target):
+def chain_rule(x_1, x_2, z_1, z_2,h_1, h_2,prediction, target):
 
     total_loss_prime = prediction - target
     
-    h_prime = 1 if z > 0 else 0
-
-    z_prime_with_respect_to_w_1 = x_1
-    z_prime_with_respect_to_w_2 = x_2
-    z_prime_with_respect_to_b = 1
-
-    gradient_slope_w_1 = total_loss_prime * h_prime * z_prime_with_respect_to_w_1
-    gradient_slope_w_2 = total_loss_prime * h_prime * z_prime_with_respect_to_w_2
-    gradient_slope_b = total_loss_prime * h_prime * z_prime_with_respect_to_b
+    h_1_prime = 1 if z_1 > 0 else 0
+    h_2_prime = 1 if z_2 > 0 else 0
 
 
-    return gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b
+    z_1_prime_with_respect_to_w_1 = x_1
+    z_1_prime_with_respect_to_w_2 = x_2
+    z_1_prime_with_respect_to_b_1 = 1
+
+    z_2_prime_with_respect_to_w_3 = x_1
+    z_2_prime_with_respect_to_w_4 = x_2
+    z_2_prime_with_respect_to_b_2 = 1
+
+    z_prime_with_respect_to_w_5 = h_1
+    z_prime_with_respect_to_w_6 = h_2
+    z_prime_with_respect_to_b_3 = 1
+    
+
+
+    # 1. Nörona giden hata, w_5 köprüsünden geçerek geri dönmeli!
+    gradient_slope_w_1 = total_loss_prime * old_w_5 * h_1_prime * z_1_prime_with_respect_to_w_1
+    gradient_slope_w_2 = total_loss_prime * old_w_5 * h_1_prime * z_1_prime_with_respect_to_w_2
+    gradient_slope_b_1 = total_loss_prime * old_w_5 * h_1_prime * z_1_prime_with_respect_to_b_1
+
+    # 2. Nörona giden hata, w_6 köprüsünden geçerek geri dönmeli!
+    gradient_slope_w_3 = total_loss_prime * old_w_6 * h_2_prime * z_2_prime_with_respect_to_w_3
+    gradient_slope_w_4 = total_loss_prime * old_w_6 * h_2_prime * z_2_prime_with_respect_to_w_4
+    gradient_slope_b_2 = total_loss_prime * old_w_6 * h_2_prime * z_2_prime_with_respect_to_b_2
+
+    gradient_slope_w_5 = total_loss_prime * z_prime_with_respect_to_w_5
+    gradient_slope_w_6 = total_loss_prime * z_prime_with_respect_to_w_6
+    gradient_slope_b_3 = total_loss_prime * z_prime_with_respect_to_b_3
+
+
+
+    return gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b_1, gradient_slope_w_3, gradient_slope_w_4, gradient_slope_b_2, gradient_slope_w_5, gradient_slope_w_6, gradient_slope_b_3
+
 
 
 # Her parametre için step sizeların hesaplanıp güncel parametlerin hesaplanması
-def backpropagation(x_1, x_2, z,target, prediction, old_w_1, old_w_2, old_b):
+def backpropagation(x_1, x_2, h_1, h_2, z_1, z_2, target, prediction, old_w_1, old_w_2, old_w_3, old_w_4, old_w_5, old_w_6, old_b_1, old_b_2, old_b_3):
 
     total_loss = loss_function(target,prediction)
-    gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b= chain_rule(x_1, x_2, z, prediction, target)
+    gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b_1, gradient_slope_w_3, gradient_slope_w_4, gradient_slope_b_2, gradient_slope_w_5, gradient_slope_w_6, gradient_slope_b_3 = chain_rule(x_1, x_2, z_1, z_2,h_1, h_2,prediction, target)
+    
     step_size_w_1 = LEARNING_RATE * gradient_slope_w_1
     step_size_w_2 = LEARNING_RATE * gradient_slope_w_2
-    step_size_b = LEARNING_RATE * gradient_slope_b
+    step_size_b_1 = LEARNING_RATE * gradient_slope_b_1
+
+    step_size_w_3 = LEARNING_RATE * gradient_slope_w_3
+    step_size_w_4 = LEARNING_RATE * gradient_slope_w_4
+    step_size_b_2 = LEARNING_RATE * gradient_slope_b_2
+
+    step_size_w_5 = LEARNING_RATE * gradient_slope_w_5
+    step_size_w_6 = LEARNING_RATE * gradient_slope_w_6
+    step_size_b_3 = LEARNING_RATE * gradient_slope_b_3
+
 
 
     new_w_1 = old_w_1 - step_size_w_1
     new_w_2 = old_w_2 - step_size_w_2
-    new_b = old_b - step_size_b
+    new_b_1 = old_b_1 - step_size_b_1
 
-    return total_loss, gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b, old_w_1, new_w_1, old_w_2, new_w_2, old_b, new_b
+    new_w_3 = old_w_3 - step_size_w_3
+    new_w_4 = old_w_4 - step_size_w_4
+    new_b_2 = old_b_2 - step_size_b_2
+
+    new_w_5 = old_w_5 - step_size_w_5
+    new_w_6 = old_w_6 - step_size_w_6
+    new_b_3 = old_b_3 - step_size_b_3
+
+
+    return total_loss, gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b_1, gradient_slope_w_3, gradient_slope_w_4, gradient_slope_b_2, gradient_slope_w_5, gradient_slope_w_6, gradient_slope_b_3, old_w_1, new_w_1, old_w_2, new_w_2, old_b_1, new_b_1, old_w_3, new_w_3, old_w_4, new_w_4, old_b_2, new_b_2, old_w_5, new_w_5, old_w_6, new_w_6, old_b_3, new_b_3
 
 
 # Modeli 100 tur eğitiyoruz
-epochs = 100
+epochs = 13
 
 for epoch in range(epochs) :
     # Her 1 turda her bütün dataseti sırayla gösteriyoruz
     for i in range(len(dataset)):
         
-        z, h, prediction, x_1, x_2= forward(dataset[i][0], dataset[i][1])
+        z_1, h_1, z_2, h_2, prediction, x_1 , x_2, = forward(dataset[i][0], dataset[i][1])
 
-        total_loss, gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b, old_w_1, new_w_1, old_w_2, new_w_2, old_b, new_b= backpropagation(x_1, x_2, z,dataset[i][2], prediction, old_w_1, old_w_2, old_b)
+        total_loss, gradient_slope_w_1, gradient_slope_w_2, gradient_slope_b_1, gradient_slope_w_3, gradient_slope_w_4, gradient_slope_b_2, gradient_slope_w_5, gradient_slope_w_6, gradient_slope_b_3, old_w_1, new_w_1, old_w_2, new_w_2, old_b_1, new_b_1, old_w_3, new_w_3, old_w_4, new_w_4, old_b_2, new_b_2, old_w_5, new_w_5, old_w_6, new_w_6, old_b_3, new_b_3 = backpropagation(x_1, x_2, z_1, z_2, h_1, h_2, dataset[i][2], prediction, old_w_1, old_w_2, old_w_3, old_w_4, old_w_5, old_w_6, old_b_1, old_b_2, old_b_3)
 
-        print(f"{epoch+1}. Tur {dataset[i]}. data için hesaplanan prediction: {prediction} \n hesaplanan toplam hata: {total_loss} \n hesaplanan w_1 gradient slope: {gradient_slope_w_1} \n hesaplanan w_2 gradient slope: {gradient_slope_w_2} \n hesaplanan b gradient slope: {gradient_slope_b} \n hesaplanan   yeni w1 değeri: {new_w_1} \n hesaplanan   yeni w2 değeri: {new_w_2} \n hesaplanan   yeni b değeri: {new_b} ")
+        print(f"{epoch+1}. Tur {dataset[i]}. data için hesaplanan prediction: {prediction} \n hesaplanan toplam hata: {total_loss} \n hesaplanan w_1 gradient slope: {gradient_slope_w_1} \n hesaplanan w_2 gradient slope: {gradient_slope_w_2} \n hesaplanan b_1 gradient slope: {gradient_slope_b_1} \n hesaplanan   yeni w1 değeri: {new_w_1} \n hesaplanan   yeni w2 değeri: {new_w_2} \n hesaplanan   yeni b_1 değeri: {new_b_1}  hesaplanan w_3 gradient slope: {gradient_slope_w_3} \n hesaplanan w_4 gradient slope: {gradient_slope_w_4} \n hesaplanan b_2 gradient slope: {gradient_slope_b_2} \n hesaplanan yeni w_3 değeri: {new_w_3} \n hesaplanan yeni w_4 değeri: {new_w_4} \n hesaplanan yeni b_2 değeri: {new_b_2} hesaplanan w_5 gradient slope: {gradient_slope_w_5} \n hesaplanan w_6 gradient slope: {gradient_slope_w_6} \n hesaplanan b_3 gradient slope: {gradient_slope_b_3} \n hesaplanan yeni w_5 değeri: {new_w_5} \n hesaplanan yeni w_6 değeri: {new_w_6} \n hesaplanan yeni b_3 değeri: {new_b_3}")
 
         old_w_1 = new_w_1
         old_w_2 = new_w_2
-        old_b = new_b
+        old_b_1 = new_b_1
+
+        old_w_3 = new_w_3
+        old_w_4 = new_w_4
+        old_b_2 = new_b_2
+
+        old_w_5 = new_w_5
+        old_w_6 = new_w_6
+        old_b_3 = new_b_3
+
 
 
 print("--------------------------------------------------")
@@ -117,19 +194,13 @@ print("Tek bir data ile eğitilmiş modeli test ediyoruz")
 
 # Modelin daha önce görmediği iki input
 new_x_1 = 20
-new_x_2 = 0
+new_x_2 = 1
 
 # Test aşamasında sadece forward metodunu çağırıyoruz
-test_z, test_h, test_prediction, _, _ = forward(new_x_1, new_x_2)
+test_z_1, test_h_1, test_z_2, test_h_2, test_prediction, _, _ = forward(new_x_1, new_x_2)
 
 print(f"Gelen Yeni Veriler -> x_1: {new_x_1}, x_2: {new_x_2}")
 print(f"Eğitilmiş Modelin Tahmini: {test_prediction}")
-
-
-
-
-
-
 
 
 
